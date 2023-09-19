1a0e19829761e2f3b323cd70abb8cec62f95a5bb
David Gjennestad <davigjenn@gmail.com>
2023-09-19 01:21:27 -0600

    Condtionals Works

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int gradeNum = int.Parse(grade);
+
+        if (gradeNum >= 90)
+        {
+            
+            Console.WriteLine("A");
+        }
+        else if (gradeNum >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (gradeNum >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (gradeNum >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
     }
 }
