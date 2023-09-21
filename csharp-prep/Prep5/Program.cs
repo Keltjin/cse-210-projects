93985ed6362cb3d36473853ffd465a5eeb716ec8
David Gjennestad <davigjenn@gmail.com>
2023-09-20 22:48:03 -0600

    Finsih Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string username = PromptUserName();
+        int userNum = PromptUserNumber();
+        int squared = SquareNumber(userNum);
+        DisplayResult(username, squared);
+
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("What is your USERNAME? ");
+        string username = Console.ReadLine();
+        return username;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        string numText = Console.ReadLine();
+        int userNum = int.Parse(numText);
+        return userNum;
+    }
+
+    static int SquareNumber(int userNum)
+    {
+        int squared = userNum * userNum;
+        return squared;
+    }
+
+    static void DisplayResult(string username, int squared)
+    {
+        Console.WriteLine($"Username: {username}, Squared Number: {squared}");
     }
 }
