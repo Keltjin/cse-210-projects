17566a1e3f688f0ddba88cbb0cc07b620f1a6cb4
David Gjennestad <davigjenn@gmail.com>
2023-09-30 10:44:11 -0600

    Update Syntext

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,24 +5,24 @@ class Program
     static void Main(string[] args)
     {   
         Job job1 = new Job();
-        job1._company = "Microsoft";
-        job1._jobTitle = "Software Engineer";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
+        job1.Company = "Microsoft";
+        job1.JobTitle = "Software Engineer";
+        job1.StartYear = 2019;
+        job1.EndYear = 2022;
 
         Job job2 = new Job();
-        job2._company = "Apple";
-        job2._jobTitle = "Manager";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
+        job2.Company = "Apple";
+        job2.JobTitle = "Manager";
+        job2.StartYear = 2022;
+        job2.EndYear = 2023;
 
-        Resume resume1 = new Resume();
-        resume1._name = "Brent Smith";
+        Resume resume = new Resume();
+        resume.Name = "Brent Smith";
 
-        resume1._jobs.Add(job1);
-        resume1._jobs.Add(job2);
+        resume.Jobs.Add(job1);
+        resume.Jobs.Add(job2);
 
-        resume1.Display();
+        resume.Display();
     }
     
 }
--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -1,19 +1,18 @@
 public class Resume 
 {
-    public string _name;
-    public List<Job> _jobs = new List<Job>();
-
+    public string Name;
+    public List<Job> Jobs;
     public Resume()
     {
-
+       this.Jobs = new List<Job>();
     }
 
     public void Display()
     {
-        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine($"Name: {Name}");
         Console.WriteLine("Jobs");
 
-        foreach (Job job in _jobs)
+        foreach (Job job in Jobs)
         {
             job.DisplayJobDetails();
         }
