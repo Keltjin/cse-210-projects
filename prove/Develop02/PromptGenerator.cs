b72057b22243db5d6fe93e020b83b4b3898c6e35
David Gjennestad <davigjenn@gmail.com>
2023-10-17 15:18:46 -0600

    Created PromptGenerator Class and attributes, working on the the DisplayPrompt method now.

---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+public class PromptGenerator
+{
+    public List<string> prompts;
+
+    public Random rand;
+
+    public PromptGenerator()
+    {
+        this.prompts = new List<string> {"Who was the most intersting person I interacted with today?",
+        "What was the best part of my day?", 
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?",
+        "How was I able to serve another person today?",
+        "How did I improve myself today?"};
+
+        this.rand = new Random();
+    }
+
+    public void DisplayPrompt()
+    {
+
+    }
+}
